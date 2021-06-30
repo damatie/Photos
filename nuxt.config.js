be949de6b69a9e980059221abba6a44917b0852e
damatie <edafemaxwell@gmail.com>
2021-06-30 23:26:46 +0100

    Page Updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css' }
+      
     ]
   },
  
