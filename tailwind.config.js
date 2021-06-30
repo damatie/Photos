cbd1005bd74eb8e3626f2564e449ad0b5990f4c7
damatie <edafemaxwell@gmail.com>
2021-06-30 22:16:42 +0100

    Updated tailwind package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: [
-    './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    // './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
